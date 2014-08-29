5dc5c658e0b8675cc7b2c511a38019b2f9002ccf
semvasyl <semvasyl@gmail.com>
2014-08-29 11:54:20 +0300

    Винесення сценаріїв у правил стилів
    виніс з головної сторінки скрипти і каскадні стилі у відповідні папки

---
+++ js/custom.js
@@ -0,0 +1,228 @@
+
+		jQuery(function($){
+			// bind event handlers to modal triggers
+			$('body').on('click', '.trigger', function(e){
+				e.preventDefault();
+				$('#test-modal').modal().open();
+			});
+
+			// attach modal close handler
+			$('.modal .close').on('click', function(e){
+				e.preventDefault();
+				$.modal().close();
+			});
+
+			// below isn't important (demo-specific things)
+			$('.modal .more-toggle').on('click', function(e){
+				e.stopPropagation();
+				$('.modal .more').toggle();
+			});
+		});
+	<!--  BEGIN JIVOSITE CODE {literal}  -->
+<script type='text/javascript'>
+(function(){ var widget_id = 'Nn98dzKKbC';
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+<!--  {/literal} END JIVOSITE CODE  -->
+
+		jQuery(function($){
+			// bind event handlers to modal triggers
+			$('body').on('click', '.trigger2', function(e){
+				e.preventDefault();
+				$('#test-modal2').modal().open();
+			});
+
+			// attach modal close handler
+			$('.modal2 .close').on('click', function(e){
+				e.preventDefault();
+				$.modal().close();
+			});
+
+			// below isn't important (demo-specific things)
+			$('.modal2 .more-toggle').on('click', function(e){
+				e.stopPropagation();
+				$('.modal2 .more').toggle();
+			});
+		});
+	<!--  BEGIN JIVOSITE CODE {literal}  -->
+<script type='text/javascript'>
+(function(){ var widget_id = 'Nn98dzKKbC';
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+<!--  {/literal} END JIVOSITE CODE  -->
+	
+		jQuery(function($){
+			// bind event handlers to modal triggers
+			$('body').on('click', '.trigger3', function(e){
+				e.preventDefault();
+				$('#test-modal3').modal().open();
+			});
+
+			// attach modal close handler
+			$('.modal3 .close').on('click', function(e){
+				e.preventDefault();
+				$.modal().close();
+			});
+
+			// below isn't important (demo-specific things)
+			$('.modal3 .more-toggle').on('click', function(e){
+				e.stopPropagation();
+				$('.modal3 .more').toggle();
+			});
+		});
+	<!--  BEGIN JIVOSITE CODE {literal}  -->
+<script type='text/javascript'>
+(function(){ var widget_id = 'Nn98dzKKbC';
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+<!--  {/literal} END JIVOSITE CODE  -->
+	
+		jQuery(function($){
+			// bind event handlers to modal triggers
+			$('body').on('click', '.trigger4', function(e){
+				e.preventDefault();
+				$('#test-modal4').modal().open();
+			});
+
+			// attach modal close handler
+			$('.modal4 .close').on('click', function(e){
+				e.preventDefault();
+				$.modal().close();
+			});
+
+			// below isn't important (demo-specific things)
+			$('.modal4 .more-toggle').on('click', function(e){
+				e.stopPropagation();
+				$('.modal4 .more').toggle();
+			});
+		});
+	<!--  BEGIN JIVOSITE CODE {literal}  -->
+<script type='text/javascript'>
+(function(){ var widget_id = 'Nn98dzKKbC';
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+<!--  {/literal} END JIVOSITE CODE  -->
+	
+		jQuery(function($){
+			// bind event handlers to modal triggers
+			$('body').on('click', '.trigger5', function(e){
+				e.preventDefault();
+				$('#test-modal5').modal().open();
+			});
+
+			// attach modal close handler
+			$('.modal5 .close').on('click', function(e){
+				e.preventDefault();
+				$.modal().close();
+			});
+
+			// below isn't important (demo-specific things)
+			$('.modal5 .more-toggle').on('click', function(e){
+				e.stopPropagation();
+				$('.modal5 .more').toggle();
+			});
+		});
+	<!--  BEGIN JIVOSITE CODE {literal}  -->
+<script type='text/javascript'>
+(function(){ var widget_id = 'Nn98dzKKbC';
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+<!--  {/literal} END JIVOSITE CODE  -->
+	
+		jQuery(function($){
+			// bind event handlers to modal triggers
+			$('body').on('click', '.trigger6', function(e){
+				e.preventDefault();
+				$('#test-modal6').modal().open();
+			});
+
+			// attach modal close handler
+			$('.modal6 .close').on('click', function(e){
+				e.preventDefault();
+				$.modal().close();
+			});
+
+			// below isn't important (demo-specific things)
+			$('.modal6 .more-toggle').on('click', function(e){
+				e.stopPropagation();
+				$('.modal6 .more').toggle();
+			});
+		});
+	<!--  BEGIN JIVOSITE CODE {literal}  -->
+<script type='text/javascript'>
+(function(){ var widget_id = 'Nn98dzKKbC';
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+<!--  {/literal} END JIVOSITE CODE  -->
+	
+		jQuery(function($){
+			// bind event handlers to modal triggers
+			$('body').on('click', '.trigger7', function(e){
+				e.preventDefault();
+				$('#test-modal7').modal().open();
+			});
+
+			// attach modal close handler
+			$('.modal7 .close').on('click', function(e){
+				e.preventDefault();
+				$.modal().close();
+			});
+
+			// below isn't important (demo-specific things)
+			$('.modal7 .more-toggle').on('click', function(e){
+				e.stopPropagation();
+				$('.modal7 .more').toggle();
+			});
+		});
+	<!--  BEGIN JIVOSITE CODE {literal}  -->
+<script type='text/javascript'>
+(function(){ var widget_id = 'Nn98dzKKbC';
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+<!--  {/literal} END JIVOSITE CODE  -->
+	
+		jQuery(function($){
+			// bind event handlers to modal triggers
+			$('body').on('click', '.trigger8', function(e){
+				e.preventDefault();
+				$('#test-modal8').modal().open();
+			});
+
+			// attach modal close handler
+			$('.modal8 .close').on('click', function(e){
+				e.preventDefault();
+				$.modal().close();
+			});
+
+			// below isn't important (demo-specific things)
+			$('.modal8 .more-toggle').on('click', function(e){
+				e.stopPropagation();
+				$('.modal8 .more').toggle();
+			});
+		});
+	<!--  BEGIN JIVOSITE CODE {literal}  -->
+<script type='text/javascript'>
+(function(){ var widget_id = 'Nn98dzKKbC';
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+<!--  {/literal} END JIVOSITE CODE  -->
+	
+		jQuery(function($){
+			// bind event handlers to modal triggers
+			$('body').on('click', '.trigger9', function(e){
+				e.preventDefault();
+				$('#test-modal9').modal().open();
+			});
+
+			// attach modal close handler
+			$('.modal9 .close').on('click', function(e){
+				e.preventDefault();
+				$.modal().close();
+			});
+
+			// below isn't important (demo-specific things)
+			$('.modal9 .more-toggle').on('click', function(e){
+				e.stopPropagation();
+				$('.modal9 .more').toggle();
+			});
+		});
+	/*<!--  BEGIN JIVOSITE CODE {literal}  -->*/
+
+(function(){ var widget_id = 'Nn98dzKKbC';
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; 
+s.src = '//code.jivosite.com/script/widget/'+widget_id; 
+var ss = document.getElementsByTagName('script')[0]; 
+ss.parentNode.insertBefore(s, ss);})();</script>
+/*<!--  {/literal} END JIVOSITE CODE  -->*/
