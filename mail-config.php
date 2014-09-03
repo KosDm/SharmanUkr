2d1913645e38e9c751904e7e0473eb6730aaf7a2
KosDm <dk@kitgroup.com.ua>
2014-09-03 17:56:30 +0300

    MailChimp delete

--- mail-config.php
+++ mail-config.php
@@ -6,6 +6,6 @@ define('YOUR_COMPANY_NAME', 'Car|Rental Landing Page');
 
 $mailchimpSupport 	= true; // set 'true' to activate mailchimp support
 
-define('MAILCHIMP_API_KEY', 'b714582d55c439ece1f49cb8d167c4b9-us3');
-define('MAILCHIMP_LIST_ID', 'f84fe4d84c');
+define('MAILCHIMP_API_KEY', '');
+define('MAILCHIMP_LIST_ID', '');
 
